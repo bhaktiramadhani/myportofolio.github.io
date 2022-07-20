db0d2c4f490d544a1aa29eba5fc31e4e8a6e4616
bhaktiramadhanii <bakti0048@gmail.com>
2022-07-20 20:03:24 +0800

    menambahkan fitur contact form

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+// contact
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbywAnZoSd9CKIUC_pHUKnzV120ZN2RsE_6jLbMsD6eWjMHwufUctwOvnSMcqgl-gzq4/exec";
+const form = document.forms["bhakti-contact-form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  Swal.fire({
+    position: "center",
+    icon: "info",
+    title: "tunggu sebentar",
+    timer: 2200,
+    timerProgressBar: true,
+  });
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      setTimeout(() => {
+        console.log("Success!", response);
+        Swal.fire(
+          "Terkirim",
+          "Pesan anda telah terkirim, Terimakasih!",
+          "success"
+        );
+        document.getElementById("nama").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("pesan").value = "";
+      });
+    }, 2000)
+    .catch((error) => console.error("Error!", error.message));
+});
